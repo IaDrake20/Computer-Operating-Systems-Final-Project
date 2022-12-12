af4f0e15973bbfc0b2949e899282eb579b103c40
IaDrake20 <iad11@pitt.edu>
2022-12-12 01:40:19 -0500

    getting core dump, will check once I've had sleep

--- elf_example.c
+++ elf_example.c
@@ -9,8 +9,9 @@ struct {
 
 struct {
 	int32_t number;
-	uint32_t other_loop;	
-} state_collatz_p1 = { .number = 0, .other_loop = UINT8_MAX}; 
+	uint32_t other_loop;
+	int32_t iteration_no;	
+} state_collatz_p1 = { .number = 0, .other_loop = UINT8_MAX, .iteration_no = 0}; 
 
 elf_status_t handler_ping(uint32_t self_id, elf_event_t event) {
 
@@ -33,7 +34,7 @@ elf_status_t handler_ping(uint32_t self_id, elf_event_t event) {
 
 	return ELF_OK;
 }
-
+		
 
 struct {
 	uint32_t other_loop;
@@ -55,7 +56,7 @@ elf_status_t handler_pong(uint32_t self_id, elf_event_t event) {
 }
 
 //Our example based on collatz conjecture... It takes in an int, if even divides by 2, otherwise multoply by 3 and add 1
-elf_status_t handler_Collatz_P1(uint32_t selfID, elf_event_t event, int passed_num, int iterations){
+elf_status_t handler_Collatz_P1(uint32_t selfID, elf_event_t event){
 	
 	if(state_collatz_p1.other_loop == UINT8_MAX){
 		state_collatz_p1.other_loop = event.value.loop_id;
@@ -64,31 +65,41 @@ elf_status_t handler_Collatz_P1(uint32_t selfID, elf_event_t event, int passed_n
 		printf("Finished calculating");
 		return ELF_OK;
 	}
-	
-	printf("Please enter a non-negative  whole number");
-	scanf("%d", &state_collatz_p1.number);
-	printf("\n");//in future just put in front of next print statement
+
+	if(state_collatz_p1.iteration_no == 0){
+
+		printf("Please enter a non-negative  whole number");
+		scanf("%d", &state_collatz_p1.number);
+		printf("\n");//in future just put in front of next print statement
+	}
 	if(state_collatz_p1.number % 2 != 0){
 		state_collatz_p1.number /= 2;
 	}
+		
+	elf_send(state_collatz_p1.other_loop, elf_event_token());
 
-	elf_send(state_collatz_p2.other_loop, elf_event_token());
-
-	if(passed_num > 0 || passed_num != 0){
+	if(state_collatz_p1.number > 0 || state_collatz_p1.number != 0){
 		printf("%d", state_collatz_p1.number);
 	}
 
-	return ELF_OKAY;
+	return ELF_OK;
 }
 
 struct {
 	uint32_t other_loop;
 } state_collatz_p2 = { .other_loop = UINT8_MAX};
 
-elf_status_t handler_Collatz_P2(uint32_t selfID, elf_event_t event, int iterations){
-	state_collatz.number *= 3;
-	state_collatz.number += 1;
-	elf_send(state_collatz_p1.other_loop, elf_token_event());
+elf_status_t handler_Collatz_P2(uint32_t selfID, elf_event_t event){
+	if(state_collatz_p2.other_loop == UINT8_MAX){
+		state_collatz_p2.other_loop = event.value.loop_id;
+		return ELF_OK;
+	}
+	state_collatz_p1.number *= 3;
+	state_collatz_p1.number += 1;
+	++state_collatz_p1.iteration_no;
+	elf_send(state_collatz_p2.other_loop, elf_event_token());
+	printf("\n%d\n", (int)state_collatz_p1.number);
+	return ELF_OK;
 }	
 
 
@@ -99,12 +110,20 @@ elf_status_t handler_main(uint32_t self_id, elf_event_t event) {
 	uint32_t ping_id = 1;  // was UINT32_MAX
 	uint32_t pong_id = 2;  // was UINT32_MAX
 
+	uint32_t collatz_p1_id = 3;
+	uint32_t collatz_p2_id = 4;
+
 	elf_init(&ping_id, handler_ping);
 	elf_init(&pong_id, handler_pong);
 
 	elf_send(pong_id, elf_event_loop_id(ping_id));
 	elf_send(ping_id, elf_event_loop_id(pong_id));
 
+	elf_init(&collatz_p1_id, handler_Collatz_P1);
+	elf_init(&collatz_p2_id, handler_Collatz_P2);
+
+	elf_send(collatz_p1_id, elf_event_loop_id(collatz_p2_id));
+	elf_send(collatz_p2_id, elf_event_loop_id(collatz_p1_id));
 	return ELF_OK; // TODO: change to ELF_OK (was ELF_ERROR)
 }
 
